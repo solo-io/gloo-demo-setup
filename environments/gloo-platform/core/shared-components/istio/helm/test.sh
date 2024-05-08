3e7434f082b11d34dd6e5eea92373bae6a03297c
Sundar Vaidyanathan <sundar.vaidyanathan@solo.io>
2024-05-08 10:04:22 -0600

    Initial Commit

---
+++ environments/gloo-platform/core/shared-components/istio/helm/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-ingressgateway-ISTIO_TAG_REVISION istio-gateways 10 ${cluster_context}
+#$SCRIPT_DIR/tools/wait-for-rollout.sh deployment istio-eastwestgateway istio-gateways 10 ${cluster_context}
